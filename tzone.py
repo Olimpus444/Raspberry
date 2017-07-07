551709e5101aaeb52348a72780dc76266c76eac1
Olimpus444 <olimpus4@windowslive.com>
2017-07-07 22:53:25 +0200

    Create tzone.py

---
+++ tzone.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+# arhn.eu subscription counter for ZEROSEG sample
+# Based on the ZEROSEG example libraries
+# This is just a poorly written sample for reference only, please keep that in mind.
+#
+# This script requires the ZeroSeg library
+# located at
+# https://github.com/AverageManVsPi/ZeroSeg
+# 
+# The Youtube and Twitch functionality
+# uses the official APIs and as such
+# require a free dev API keys.
+# These need to be filled in the
+# respective variables (lines 30 and 59)
+
+import ZeroSeg.led as led
+import time
+import random
+from datetime import datetime
+import urllib, json
+import RPi.GPIO as GPIO
+import threading
+switch1 = 17
+switch2 = 26
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(switch1, GPIO.IN)
+GPIO.setup(switch2, GPIO.IN) 
+
+tick = 0;
+auto = 0;
+		
+def autogo():
+	global mode
+	threading.Timer(10.0, autogo).start()
+	global auto
+	if auto == 1:
+		mode += 1
+		if mode == 3:
+			mode = 1
+		print "Auto Switch: ", mode
+		
+		
+device = led.sevensegment(cascaded=2)
+
+autogo();
+
+
+time.sleep(2)
+mode = 1;
+level = 1;
+device.brightness(level)
+refresh = 99;
+anim = 8;		
+
+
+while True:
+	now = datetime.now()
+	if mode == 1:
+		hour = now.hour
+		minute = now.minute
+		second = now.second
+		dot = second % 2 == 0
+		# Set hours
+		device.letter(1, 8, int(hour / 10))     # Tens
+		device.letter(1, 7, hour % 10)     # Ones
+		device.letter(1, 6, " ", 1)
+		# Set minutes
+		device.letter(1, 5, int(minute / 10))   # Tens
+		device.letter(1, 4, minute % 10)        # Ones
+		device.letter(1, 3, " ", 1)
+		# Set seconds
+		device.letter(1, 2, int(second / 10))   # Tens
+		device.letter(1, 1, second % 10)        # Ones
+	if mode == 2:
+		day = now.day
+		month = now.month
+		year = now.year - 2000
+
+		# Set day
+		device.letter(1, 8, int(day / 10))     # Tens
+		device.letter(1, 7, day % 10)          # Ones
+		device.letter(1, 6, '-')               # dash
+		# Set day
+		device.letter(1, 5, int(month / 10))     # Tens
+		device.letter(1, 4, month % 10)     # Ones
+		device.letter(1, 3, '-')               # dash
+		# Set day
+		device.letter(1, 2, int(year / 10))     # Tens
+		device.letter(1, 1, year % 10)     # Ones
+	if mode == 3:
+		auto = 1;	
+		mode = 1;
+	
+	if not GPIO.input(switch2):
+		if auto == 1:
+			auto = 0
+			mode = 1
+			print "Auto Off"
+		elif mode < 4:
+			mode += 1
+		else:
+			mode = 1
+		if mode == 1:
+			device.write_text(1, "CZAS")
+		if mode == 2:
+			device.write_text(1, "DATA")
+		if mode == 3:
+			device.write_text(1, "AUTO")
+		time.sleep(1)
+	#wskaznik jasnosci
+	elif not GPIO.input(switch1):
+		if level <= 2:
+			level = 5
+		elif level == 5:
+			level = 10
+		elif level == 10:
+			level = 14
+		elif level >= 14:
+			level = 1
+		device.brightness(level)
+		print "Poziom jasnosci:", level
+		time.sleep(0.5);
+	else:
+		pass
