e2e3ab42721434f9e81eb6179970f56138928ea7
Konrad <15710231+konrad4@users.noreply.github.com>
2020-12-26 18:43:38 +0100

    Add redner when music is playing

--- finalv1.py
+++ finalv1.py
@@ -41,93 +41,54 @@ autogo()
 
 
 time.sleep(2)
-mode = 1;
-level = 1;
+mode = 1
+level = 1
 device.brightness(level)
-refresh = 99;
-anim = 8;		
+refresh = 99
+anim = 8	
 
 def set_interval(func, sec):
     def func_wrapper():
         set_interval(func, sec)
-        func()
+        func
     t = threading.Timer(sec, func_wrapper)
     t.start()
     return t
 
 def get_song():
-	with urllib.request('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=Olimpus444&api_key=5c7c79f93961f31c57d4f2cf349e027a&format=json&nowplaying=true') as response:
-		response = response.read()
-		artistAndTitle = response.recenttracks.track[0].name + response.recenttracks.track[0].artist['#text']
-
-		device.show_message(artistAndTitle, delay=0.1)
-
-while True:
-	if mode == 1:
-		set_interval(get_song(), 10)
-	# now = datetime.now()
-	# if mode == 1:
-	# 	hour = now.hour
-	# 	minute = now.minute
-	# 	second = now.second
-	# 	dot = second % 2 == 0
-	# 	# Set hours
-	# 	device.letter(1, 8, int(hour / 10))     # Tens
-	# 	device.letter(1, 7, hour % 10)     # Ones
-	# 	device.letter(1, 6, " ", 1)
-	# 	# Set minutes
-	# 	device.letter(1, 5, int(minute / 10))   # Tens
-	# 	device.letter(1, 4, minute % 10)        # Ones
-	# 	device.letter(1, 3, " ", 1)
-	# 	# Set seconds
-	# 	device.letter(1, 2, int(second / 10))   # Tens
-	# 	device.letter(1, 1, second % 10)        # Ones
-	# if mode == 2:
-	# 	day = now.day
-	# 	month = now.month
-	# 	year = now.year - 2000
-
-	# 	# Set day
-	# 	device.letter(1, 8, int(day / 10))     # Tens
-	# 	device.letter(1, 7, day % 10)          # Ones
-	# 	device.letter(1, 6, '-')               # dash
-	# 	# Set day
-	# 	device.letter(1, 5, int(month / 10))     # Tens
-	# 	device.letter(1, 4, month % 10)     # Ones
-	# 	device.letter(1, 3, '-')               # dash
-	# 	# Set day
-	# 	device.letter(1, 2, int(year / 10))     # Tens
-	# 	device.letter(1, 1, year % 10)     # Ones
-	if mode == 3:
-		auto = 1;	
-		mode = 1;
+	global mode
+	url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=Olimpus444&api_key=5c7c79f93961f31c57d4f2cf349e027a&format=json&limit=1&nowplaying=true'
+	data = json.load(urllib.request(url))
+	if 'recenttracks' in data:
+		if 'track' in data['recenttracks']:
+			if '@attr' in data['recenttracks']['track'][0]:
+				print('jest')
+				mode = 1
+				artistAndTitle = data['recenttracks']['track'][0]['name'] + ' --BY-- ' + data['recenttracks']['track'][0]['artist']['#text']
+				return artistAndTitle
+			else:
+				mode = 2
 	
-	if not GPIO.input(switch2):
-		if auto == 1:
-			auto = 0
-			mode = 1
-			print "Auto Off"
-		elif mode < 4:
-			mode += 1
-		else:
-			mode = 1
-		if mode == 1:
-			device.write_text(1, "CZAS")
-		if mode == 2:
-			device.write_text(1, "DATA")
-		if mode == 3:
-			device.write_text(1, "AUTO")
-		time.sleep(1)
-	#wskaznik jasnosci
-	elif not GPIO.input(switch1):
-		if level <= 2:
-			level = 5
-		elif level == 5:
-			level = 10
-		elif level == 10:
-			level = 14
-		elif level >= 14:
-			level = 1
-		device.brightness(level)
-		print "Poziom jasnosci:", level
-		time.sleep(0.5);
+def render_content():
+	songString = get_song()
+	if (mode == 1):
+		device.show_message(songString.upper())
+	else:
+		now = datetime.now()
+		hour = now.hour
+		minute = now.minute
+		second = now.second
+		dot = second % 2 == 0
+		# Set hours
+		device.letter(1, 8, int(hour / 10))     # Tens
+		device.letter(1, 7, hour % 10)     # Ones
+		device.letter(1, 6, " ", 1)
+		# Set minutes
+		device.letter(1, 5, int(minute / 10))   # Tens
+		device.letter(1, 4, minute % 10)        # Ones
+		device.letter(1, 3, " ", 1)
+		# Set seconds
+		device.letter(1, 2, int(second / 10))   # Tens
+		device.letter(1, 1, second % 10)        # Ones
+
+set_interval(render_content(), 30)
